d88722b2d6b6a31a1ca448995a11e10912e8767f
hanako.toshima <factor_12cha@yahoo.co.jp>
2018-09-25 13:49:27 +0000

    bootstrap design

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -5,25 +5,21 @@
 <!-- ここにページ毎のコンテンツを書く -->
     <h1>メッセージ新規作成ページ</h1>
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8">
-        <div class="col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
 
-        {!! Form::model($message, ['route' => 'messages.store']) !!}
-        <div class="form-group">
-            {!! Form::label('status', 'ステータス:') !!}
-            {!! Form::text('status', null, ['class' => 'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('content', 'メッセージ:') !!}
-            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            {!! Form::model($message, ['route' => 'messages.store']) !!}
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
             
-            {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
-            {!! Form::close() !!}
-        </div>
-        </div>
-        </div>
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -5,10 +5,8 @@
 <!-- ここにページ毎のコンテンツを書く -->
     <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8">
-        <div class="col-lg-offset-3 col-lg-6">
-
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+            
         {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
             <div class="form-group">
                 {!! Form::label('status', 'ステータス:') !!}
@@ -20,8 +18,6 @@
             </div>
 
             {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
-        </div>
-        </div>
         </div>
     </div>
     {!! Form::close() !!}
