12e05a5ec8bb8309a47dc0d417d6f9fff1d2c3f7
hanako.toshima <factor_12cha@yahoo.co.jp>
2018-10-06 11:46:04 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::resource('messages', 'MessagesController');
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
